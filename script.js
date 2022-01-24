fcf7185c0e292d99d988669c9661fd92ad550afc
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-01-24 17:02:30 +0100

    WIP on displaying the correct info

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
 import Data from "/config.js";
 
-console.log(Data.key);
-
-const dateMessage = document.getElementById("dateMessage");
-const temperatureMessage = document.getElementById("temperatureMessage");
 const searchBar = document.getElementById("searchBar");
 const submitBtn = document.getElementById("submitBtn");
-
+const mainTag = document.getElementsByTagName("main")[0];
 
 submitBtn.addEventListener("click", () => {
-
+    mainTag.innerHTML= ""; //Make sure the previous searchresults will dissapear
     let searchInput = searchBar.value.toLowerCase();
     fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + searchInput + '&appid=' + Data.key)
         .then(response => response.json())
         .then(data => {
-            const mainTag = document.getElementsByTagName("main")[0];
-            for (let i = 0; i< data.list.length; i++) {
-            console.log(data.list.length);
+            console.log(data);
+            for (let i = 0; i< 8; i++) {
             const newDate = document.createElement("p");
             newDate.innerHTML = data.list[i].dt_txt;
             mainTag.appendChild(newDate);
             const temperatureParagraph = document.createElement("p");
-            temperatureParagraph.innerHTML = data.list[i].main.temp-273.15;
+            temperatureParagraph.innerHTML = Math.round(data.list[i].main.temp-273.15) + "°C" + " " + data.list[i].weather[0].description;
             mainTag.appendChild(temperatureParagraph);
             // dateMessage.innerHTML = data.list[0].dt_txt;
             // temperatureMessage.innerHTML = data.list[0].main.temp-273.15;
-        }});
-
+            
+        }
+    for (let i = 8; i< 16; i++) {
+            const newDate = document.createElement("p");
+            newDate.innerHTML = data.list[i].dt_txt;
+            mainTag.appendChild(newDate);
+            const temperatureParagraph = document.createElement("p");
+            temperatureParagraph.innerHTML = Math.round(data.list[i].main.temp-273.15) + "°C" + " " + data.list[i].weather[0].description;
+            mainTag.appendChild(temperatureParagraph);
+    }
+    });
 });
